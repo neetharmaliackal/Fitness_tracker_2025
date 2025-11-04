bb44a15270e74e389df35e6931326c75cbfdfd9f
neetharmaliackal <neethamaliackal@gmail.com>
2025-11-04 16:41:28 +1300

    codes  for activities added

--- api/urls.py
+++ api/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
+from rest_framework.routers import DefaultRouter
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from .views import RegisterView, LogoutView
+from .views import RegisterView, LogoutView, ActivityViewSet
+
+# Router for activity endpoints
+router = DefaultRouter()
+router.register(r'activities', ActivityViewSet, basename='activity')
 
 urlpatterns = [
     path('auth/register/', RegisterView.as_view(), name='register'),
@@ -9,4 +15,9 @@ urlpatterns = [
     # ✅ Refresh token endpoint
     path('auth/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('auth/logout/', LogoutView.as_view(), name='logout'),
+
+
 ]
+
+urlpatterns += router.urls
+
